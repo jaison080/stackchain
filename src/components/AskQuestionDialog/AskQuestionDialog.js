7d3eee343b12941168f1b79a75653d24634550e0
jaison080 <jaisondennis080@gmail.com>
2023-02-25 11:53:43 +0530

    Add Ask Question Dialog

---
+++ src/components/AskQuestionDialog/AskQuestionDialog.js
@@ -0,0 +1,58 @@
+import { Dialog, DialogContent } from "@mui/material";
+import styles from "./AskQuestionDialog.module.css";
+import React from "react";
+
+function AskQuestionDialog({ open, handleClose }) {
+  return (
+    <Dialog
+      open={open}
+      PaperProps={{
+        style: {
+          backgroundColor: "#02121c",
+          color: "#f9f9f9",
+          borderRadius: "1rem",
+          padding: "1rem",
+        },
+      }}
+      BackdropProps={{
+        style: {
+          opacity: 0.5,
+          background:
+            "linear-gradient(90deg, #0C4C82 -13.51%, #0D4F84 -12.59%, #187BA2 5.14%, #1F9BB8 20.99%, #24AFC5 34.24%, #26B6CA 43.28%, #30B9C7 50.27%, #4DBFBE 62.37%, #7ACBAF 78.1%, #B1D89E 94.53%)",
+        },
+      }}
+      onClose={handleClose}
+      fullWidth={true}
+      maxWidth="lg"
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogContent sx={{ "&::-webkit-scrollbar": { display: "none" } }}>
+        <div className={styles.ask_question_dialog__title}>Ask Question</div>
+        <div className={styles.ask_question_dialog__form}>
+          <div className={styles.ask_question_dialog__description}>
+            <div className={styles.ask_question_dialog__description__title}>
+              Question Title
+            </div>
+            <input type="text" />
+          </div>
+          <div className={styles.ask_question_dialog__description}>
+            <div className={styles.ask_question_dialog__description__title}>
+              Question Description
+            </div>
+            <textarea name="" id="" cols="30" rows="5"></textarea>
+          </div>
+          <div className={styles.ask_question_dialog__description}>
+            <div className={styles.ask_question_dialog__description__title}>
+              Tags {"( Seperate by , )"}
+            </div>
+            <input type="text" />
+          </div>
+          <div className={styles.ask_question_dialog__button}>Ask Question</div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default AskQuestionDialog;
--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import QuestionCard from "../QuestionCard/QuestionCard";
 import styles from "./Questions.module.css";
+import { RiQuestionAnswerLine } from "react-icons/ri";
+import AskQuestionDialog from "../AskQuestionDialog/AskQuestionDialog";
 
 function Questions() {
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
     <>
       <div className={styles.questions}>
+        <div className={styles.ask_btn} onClick={handleOpen}>
+          <RiQuestionAnswerLine />
+          Ask a Question
+        </div>
         <div className={styles.questions__title}>Questions Asked</div>
         <QuestionCard />
         <QuestionCard />
         <QuestionCard />
       </div>
+      <AskQuestionDialog open={open} handleClose={handleClose} />
     </>
   );
 }
